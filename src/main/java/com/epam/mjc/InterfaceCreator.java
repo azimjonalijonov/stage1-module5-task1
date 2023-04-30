bb1bed509d76dd31c4c929dae64cecf09643ba8b
azimjonalijonov <115147667+azimjonalijonov@users.noreply.github.com>
2023-04-30 18:55:57 +0500

    Update InterfaceCreator.java

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,32 +1,50 @@
+// package com.epam.mjc;
+
+// import java.util.List;
+// import java.util.Map;
+// import java.util.function.BiFunction;
+// import java.util.function.Consumer;
+// import java.util.function.Function;
+// import java.util.function.Predicate;
+// import java.util.function.Supplier;
+
+// public class InterfaceCreator {
+
+//     public Predicate<List<String>> isValuesStartWithUpperCase() {
+//         throw new UnsupportedOperationException("You should implement this method.");
+//     }
+
+//     public Consumer<List<Integer>> addEvenValuesAtTheEnd() {
+//         throw new UnsupportedOperationException("You should implement this method.");
+//     }
+
+//     public Supplier<List<String>> filterCollection(List<String> values) {
+//         throw new UnsupportedOperationException("You should implement this method.");
+//     }
+
+//     public Function<List<String>, Map<String, Integer>> stringSize() {
+//         throw new UnsupportedOperationException("You should implement this method.");
+//     }
+
+//     public BiFunction<List<Integer>, List<Integer>, List<Integer>> concatList() {
+//         throw new UnsupportedOperationException("You should implement this method.");
+//     }
+// }
 package com.epam.mjc;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.function.BiFunction;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 public class InterfaceCreator {
 
-    public Predicate<List<String>> isValuesStartWithUpperCase() {
-        throw new UnsupportedOperationException("You should implement this method.");
-    }
+    public Operation<Integer> divideBy(Integer divider) {
+        return x -> {
+            List<Integer> list = new ArrayList<>();
+            for (Integer i : x) {
+                list.add(i / divider);
+            }
+            return list;
 
-    public Consumer<List<Integer>> addEvenValuesAtTheEnd() {
-        throw new UnsupportedOperationException("You should implement this method.");
-    }
-
-    public Supplier<List<String>> filterCollection(List<String> values) {
-        throw new UnsupportedOperationException("You should implement this method.");
-    }
-
-    public Function<List<String>, Map<String, Integer>> stringSize() {
-        throw new UnsupportedOperationException("You should implement this method.");
-    }
-
-    public BiFunction<List<Integer>, List<Integer>, List<Integer>> concatList() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        };
     }
 }
