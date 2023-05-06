4e7c91fd2d5b1be96a6427fa8975e270763c950e
azimjonalijonov <115147667+azimjonalijonov@users.noreply.github.com>
2023-05-06 23:22:17 +0500

    Update InterfaceCreator.java

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,50 +1,68 @@
-// package com.epam.mjc;
-
-// import java.util.List;
-// import java.util.Map;
-// import java.util.function.BiFunction;
-// import java.util.function.Consumer;
-// import java.util.function.Function;
-// import java.util.function.Predicate;
-// import java.util.function.Supplier;
-
-// public class InterfaceCreator {
-
-//     public Predicate<List<String>> isValuesStartWithUpperCase() {
-//         throw new UnsupportedOperationException("You should implement this method.");
-//     }
-
-//     public Consumer<List<Integer>> addEvenValuesAtTheEnd() {
-//         throw new UnsupportedOperationException("You should implement this method.");
-//     }
-
-//     public Supplier<List<String>> filterCollection(List<String> values) {
-//         throw new UnsupportedOperationException("You should implement this method.");
-//     }
-
-//     public Function<List<String>, Map<String, Integer>> stringSize() {
-//         throw new UnsupportedOperationException("You should implement this method.");
-//     }
-
-//     public BiFunction<List<Integer>, List<Integer>, List<Integer>> concatList() {
-//         throw new UnsupportedOperationException("You should implement this method.");
-//     }
-// }
 package com.epam.mjc;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.*;
 
 public class InterfaceCreator {
 
-    public Operation<Integer> divideBy(Integer divider) {
+    public Predicate<List<String>> isValuesStartWithUpperCase() {
         return x -> {
-            List<Integer> list = new ArrayList<>();
-            for (Integer i : x) {
-                list.add(i / divider);
+            boolean result = true;
+            for (String e : x) {
+                char str = e.charAt(0);
+                if (!Character.isUpperCase(str)) {
+                    result = false;
+                }
             }
-            return list;
+            return result;
+        };
+    }
 
+    public Consumer<List<Integer>> addEvenValuesAtTheEnd() {
+        return x -> {
+            int length = x.size();
+            for (int i = 0; i < length; i++) {
+                if (x.get(i) % 2 == 0) {
+                    x.add(x.get(i));
+                }
+            }
+        };
+    }
+
+    public Supplier<List<String>> filterCollection(List<String> values) {
+        return () -> {
+            List<String> result = new ArrayList<>();
+            for (String e : values) {
+                char e_char = e.charAt(0);
+                char e_last_char = e.charAt(e.length() - 1);
+                String[] e_split = e.split(" ");
+                if (Character.isUpperCase(e_char) && e_last_char == '.' && e_split.length > 3) {
+                    result.add(e);
+                }
+            }
+            return result;
+        };
+    }
+
+    public Function<List<String>, Map<String, Integer>> stringSize() {
+        return x -> {
+            Map<String, Integer> map = new LinkedHashMap<>();
+            for (String e : x) {
+                map.put(e, e.length());
+            }
+            return map;
+        };
+    }
+
+    public BiFunction<List<Integer>, List<Integer>, List<Integer>> concatList() {
+        return (list1, list2) -> {
+            List<Integer> list = new ArrayList<>();
+            list.addAll(list1);
+            list.addAll(list2);
+            return list;
         };
     }
 }
